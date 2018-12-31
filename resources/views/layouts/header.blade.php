c762001ac4062bf266c1d884db10f2acad6517ca
Unknown <alan_hernandezgarcia@hotmail.com>
2018-12-31 00:27:43 -0600

    Quitando modo DEBUG

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,8 +6,8 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 	<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>
 	<style type="text/css">
 		.navbar{
 			margin: 3rem;
@@ -27,13 +27,12 @@
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>                        
+					<span class="icon-bar"></span>
 				</button>
 				<a class="navbar-brand" href="/">Escuela</a>
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
 				<ul class="nav navbar-nav">
-				<!--li class="active"><a href="#">INSERTAR</a></li-->
 				<li><a href="/create">INSERTAR</a></li>
 				<li><a href="/read">BUSCAR</a></li>
 				<li><a href="/update">ACTUALIZAR</a></li>
