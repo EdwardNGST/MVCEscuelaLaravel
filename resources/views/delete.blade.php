4f06b37a0744b82fc8822199c8dbc8ec0d6b6d4d
Unknown <alan_hernandezgarcia@hotmail.com>
2018-12-31 00:22:21 -0600

    Pagina completa (Pequeños detallitos)

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -1,125 +1,125 @@
 @extends('layouts.header')
 @section('content')
 <div class="container">
-<form class="form-horizontal" id="form_delete">
-			<div class="form-group">
+	<form class="form-horizontal" id="form_delete">
+		<div class="form-group">
 			<label class="control-label col-sm-2" for="ncontrol">Numero de Control:</label>
 			<div class="col-sm-10">
 				<input type="number" class="form-control" id="ncontrol" placeholder="Ingrese su numero de control" name="nc">
 			</div>
 		</div>
-    <div id="search_alert"></div>
+		<div id="search_alert"></div>
 		<hr>
 		<div class="well well-sm">
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Nombre del Estudiante:</label>
-    <div class="col-sm-10">
-      <p class="form-control-static" id="name">---Nombre del Estudiante---</p>
-    </div>
-    </div>
-      <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Carrera:</label>
-    <div class="col-sm-10">
-      <p class="form-control-static" id="career">---Carrera del Estudiante---</p>
-    </div>
-    </div>
-          <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Edad:</label>
-    <div class="col-sm-10">
-      <p class="form-control-static" id="age">---Edad del Estudiante---</p>
-    </div>
-    </div>
-              <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Telefono:</label>
-    <div class="col-sm-10">
-      <p class="form-control-static" id="phone">---Telefono del Estudiante---</p>
-    </div>
-    </div>
-    <div class="form-group">        
-			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-danger">Eliminar</button>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Nombre del Estudiante:</label>
+				<div class="col-sm-10">
+					<p class="form-control-static" id="name">---Nombre del Estudiante---</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Carrera:</label>
+				<div class="col-sm-10">
+					<p class="form-control-static" id="career">---Carrera del Estudiante---</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Edad:</label>
+				<div class="col-sm-10">
+					<p class="form-control-static" id="age">---Edad del Estudiante---</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Telefono:</label>
+				<div class="col-sm-10">
+					<p class="form-control-static" id="phone">---Telefono del Estudiante---</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-danger">Eliminar</button>
+				</div>
 			</div>
 		</div>
-  </div>
-</form>
-<div id="delete_alert"></div>
+	</form>
+	<div id="delete_alert"></div>
 </div>
 <script type="text/javascript">
-  var ncontrol="";
-  var found=false;
-  $('#ncontrol').on('keyup',function(){
-      var numC=$(this).val();
-      searchByNC(numC);
-  });
-  function searchByNC(nc){
-      $.ajax({
-        type : 'get',
-        url : '{{URL::to('searchStudent')}}',
-        data:{'nc':nc},
-        success:function(data){
-          if (data!="") {
-            found=true;
-            ncontrol=nc;
-            $('#name').html(data.nameStudent);
-            $("#career").html(data.career);
-            $('#age').html(data.age);
-            $('#phone').html(data.phone);
-            $('#search_alert').html('<div class="alert alert-success">Alumno encontrado!</div>');
-          }else{
-            $('#search_alert').html('<div class="alert alert-warning">No se encuentra el alumno que busca</div>');
-          }
-        },
-        beforeSend:function(data){
-          $('#search_alert').html('<div class="alert alert-warning">Buscando</div>');
-        }
-      });
-    event.preventDefault();
-  }
-  $( "#form_delete" ).submit(function( event ) {
-    if (ncontrol!="") {
-      $.ajax({
-        type : 'get',
-        url : '{{URL::to('deleteStudent')}}',
-        data:{'nc':ncontrol},
-        success:function(data){
-          if (data=="1") {
-            $('#delete_alert').html('<div class="alert alert-success">Alumno eliminado correctamente</div>');
-              clean();
-          }else{
-            $('#delete_alert').html('<div class="alert alert-warning">Ocurrio un problema al intentar eliminar el estudiante</div>');
-          }
-        },
-        beforeSend:function(data){
-          $('#delete_alert').html('<div class="alert alert-warning">Eliminando...</div>');
-        }
-      });
-    }else{
-      alert("Ingrese un numero de control correctamente");
-    }
-    event.preventDefault();
-  });
-  function clean(){
-            $('#ncontrol').val("");
-            $('#name').html("---Nombre del Estudiante---");
-            $("#career").html("---Carrera del Estudiante---");
-            $('#age').html("---Edad del Estudiante---");
-            $('#phone').html("---Telefono del Estudiante---");
-            var ncontrol="";
-            var found=false;
-                        $('#search_alert').html('');
-  }
-$( document ).ready(function() {
-    var nc=getParameterByName('nc');
-    if (nc!="") {
-      $('#ncontrol').val(nc);
-      searchByNC(nc);
-    }
-});
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+	var ncontrol="";
+	var found=false;
+	$('#ncontrol').on('keyup',function(){
+		var numC=$(this).val();
+		searchByNC(numC);
+	});
+	function searchByNC(nc){
+		$.ajax({
+			type : 'get',
+			url : '{{URL::to('searchStudent')}}',
+			data:{'nc':nc},
+			success:function(data){
+				if (data!="") {
+					found=true;
+					ncontrol=nc;
+					$('#name').html(data.nameStudent);
+					$("#career").html(data.career);
+					$('#age').html(data.age);
+					$('#phone').html(data.phone);
+					$('#search_alert').html('<div class="alert alert-success">Alumno encontrado!</div>');
+				}else{
+					$('#search_alert').html('<div class="alert alert-warning">No se encuentra el alumno que busca</div>');
+				}
+			},
+			beforeSend:function(data){
+				$('#search_alert').html('<div class="alert alert-warning">Buscando</div>');
+			}
+		});
+		event.preventDefault();
+	}
+	$( "#form_delete" ).submit(function( event ) {
+		if (ncontrol!="") {
+		$.ajax({
+			type : 'get',
+			url : '{{URL::to('deleteStudent')}}',
+			data:{'nc':ncontrol},
+			success:function(data){
+				if (data=="1") {
+					$('#delete_alert').html('<div class="alert alert-success">Alumno eliminado correctamente</div>');
+					clean();
+				}else{
+					$('#delete_alert').html('<div class="alert alert-warning">Ocurrio un problema al intentar eliminar el estudiante</div>');
+				}
+			},
+			beforeSend:function(data){
+				$('#delete_alert').html('<div class="alert alert-warning">Eliminando...</div>');
+			}
+		});
+		}else{
+			alert("Ingrese un numero de control correctamente");
+		}
+		event.preventDefault();
+	});
+	function clean(){
+		$('#ncontrol').val("");
+		$('#name').html("---Nombre del Estudiante---");
+		$("#career").html("---Carrera del Estudiante---");
+		$('#age').html("---Edad del Estudiante---");
+		$('#phone').html("---Telefono del Estudiante---");
+		var ncontrol="";
+		var found=false;
+		$('#search_alert').html('');
+	}
+	$( document ).ready(function() {
+		var nc=getParameterByName('nc');
+		if (nc!="") {
+			$('#ncontrol').val(nc);
+			searchByNC(nc);
+		}
+	});
+	function getParameterByName(name) {
+		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	}
 </script>
 @stop
--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,153 +1,154 @@
 @extends('layouts.header')
 @section('content')
 <div class="container">
-	<h2>Modificar alumno</h2>
-	<form id="form_search" class="form-horizontal">
-		<div class="form-group">
-			<label class="control-label col-md-2 col-sm-12" for="search">Busqueda:</label>
-			<div class="col-md-9 col-sm-11">          
-				<input type="number" class="form-control" id="nc" placeholder="Ingrese el numero de control" name="search">
-			</div>
-			<input type="submit" class="btn btn-default col-md-1 col-sm-1" id="btnSearch" value="Buscar">
-		</div>
-		<div id="search_alert"></div>
-	</form>
-	<hr>
-	<form class="form-horizontal" id="form_update">
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="name">Nombre del Estudiante:</label>
-			<div class="col-sm-10">          
-				<input type="text" class="form-control" id="name" placeholder="Ingrese su nombre" name="name">
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="career">Carrera:</label>
-			<div class="col-sm-10">
-			    <select class="form-control" id="career" name="career">
-			      <option>Seleccione su Carrera</option>
-			      <option>Administración</option>
-			      <option>Arquitectura</option>
-			      <option>Contador Público</option>
-			      <option>Electromecánica</option>
-			      <option>Gestión Empresarial</option>
-			      <option>Sistemas Computacionales</option>
-			      <option>ITICS</option>
-			    </select>
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="age">Edad:</label>
-			<div class="col-sm-10">          
-				<input type="number" class="form-control" id="age" placeholder="Ingrese su edad" name="age">
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="phone">Telefono:</label>
-			<div class="col-sm-10">          
-				<input type="number" class="form-control" id="phone" placeholder="Ingrese su telefono" name="phone">
-			</div>
-		</div>
-		<div class="form-group">        
-			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-warning">Actualizar</button>
-			</div>
-		</div>
-	</form>
-	<div id="update_alert"></div>
+    <h2>Modificar alumno</h2>
+    <form id="form_search" class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label col-md-2 col-sm-12" for="search">Busqueda:</label>
+            <div class="col-md-9 col-sm-11">
+                <input type="number" class="form-control" id="nc" placeholder="Ingrese el numero de control" name="search">
+            </div>
+            <input type="submit" class="btn btn-default col-md-1 col-sm-1" id="btnSearch" value="Buscar">
+        </div>
+        <div id="search_alert"></div>
+    </form>
+    <hr>
+    <form class="form-horizontal" id="form_update">
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="name">Nombre del Estudiante:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" placeholder="Ingrese su nombre" name="name">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="career">Carrera:</label>
+            <div class="col-sm-10">
+                <select class="form-control" id="career" name="career">
+                    <option>Seleccione su Carrera</option>
+                    <option>Administración</option>
+                    <option>Arquitectura</option>
+                    <option>Contador Público</option>
+                    <option>Electromecánica</option>
+                    <option>Gestión Empresarial</option>
+                    <option>Sistemas Computacionales</option>
+                    <option>ITICS</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="age">Edad:</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="age" placeholder="Ingrese su edad" name="age">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="phone">Telefono:</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="phone" placeholder="Ingrese su telefono" name="phone">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-warning">Actualizar</button>
+            </div>
+        </div>
+    </form>
+    <div id="update_alert"></div>
 </div>
 <script type="text/javascript">
-	var ncontrol="";
-	var found=false;
-	$( "#form_search" ).submit(function( event ) {
-		ncontrol=$('#nc').val();
-		searchByNC(ncontrol);
-	});
-	$('#nc').on('keyup',function(){
-		ncontrol=$(this).val();
-		searchByNC(ncontrol);
-	})
-	$( "#form_update" ).submit(function( event ) {
-		if (found) {
-			name=$(this.name).val();
-			career=$(this.career).val();
-			age=$(this.age).val();
-			phone=$(this.phone).val();
-			updateStudent(ncontrol, name, career, age, phone);
-		}else{
-			alert("Ingrese un numero de control correctamente");
-		}
-		event.preventDefault();
-	});
-	function searchByNC(nc){
-	    $.ajax({
-	      type : 'get',
-	      url : '{{URL::to('searchStudent')}}',
-	      data:{'nc':nc},
-	      success:function(data){
-		      if (data!="") {
-		      	found=true;
-		     	$('#name').val(data.nameStudent);
-		     	$("#career").val(data.career);
-		     	$('#age').val(data.age);
-		     	$('#phone').val(data.phone);
-		      	$('#search_alert').html('<div class="alert alert-success">Alumno encontrado!</div>');
-		      }else{
-		      	$('#search_alert').html('<div class="alert alert-warning">No se encuentra el alumno que busca</div>');
-		      }
-	      },
-	      beforeSend:function(data){
-	      	$('#search_alert').html('<div class="alert alert-warning">Buscando</div>');
-	      }
-	    });
-		event.preventDefault();
-	}
-	function updateStudent(nc, name, career, age, phone){
+    var ncontrol="";
+    var found=false;
+    $( "#form_search" ).submit(function( event ) {
+        ncontrol=$('#nc').val();
+        searchByNC(ncontrol);
+    });
+    $('#nc').on('keyup',function(){
+        ncontrol=$(this).val();
+        searchByNC(ncontrol);
+    })
+    $( "#form_update" ).submit(function( event ) {
+        if (found) {
+            name=$(this.name).val();
+            career=$(this.career).val();
+            age=$(this.age).val();
+            phone=$(this.phone).val();
+            updateStudent(ncontrol, name, career, age, phone);
+        }else{
+            alert("Ingrese un numero de control correctamente");
+        }
+        event.preventDefault();
+    });
+    function searchByNC(nc){
+        $.ajax({
+            type : 'get',
+            url : '{{URL::to('searchStudent')}}',
+            data:{'nc':nc},
+            success:function(data){
+                if (data!="") {
+                    found=true;
+                    $('#name').val(data.nameStudent);
+                    $("#career").val(data.career);
+                    $('#age').val(data.age);
+                    $('#phone').val(data.phone);
+                    $('#search_alert').html('<div class="alert alert-success">Alumno encontrado!</div>');
+                }else{
+                    $('#search_alert').html('<div class="alert alert-warning">No se encuentra el alumno que busca</div>');
+                }
+            },
+            beforeSend:function(data){
+                $('#search_alert').html('<div class="alert alert-warning">Buscando</div>');
+            }
+        });
+        event.preventDefault();
+    }
+    function updateStudent(nc, name, career, age, phone){
         var vars = {
-	        "nc": nc,
-	        "name": name,
-	        "career": career,
-	        "age": age,
-	        "phone": phone
+            "nc": nc,
+            "name": name,
+            "career": career,
+            "age": age,
+            "phone": phone
         };
-	    $.ajax({
-	      type : 'get',
-	      url : '{{URL::to('updateStudent')}}',
-	      data:vars,
-	      success:function(data){
-		      if (data=="1") {
-		      	$('#update_alert').html('<div class="alert alert-success">Datos actualizados correctamente</div>');
-		      	clean();
-		      }else{
-		      	$('#update_alert').html('<div class="alert alert-warning">Ocurrio un problema con la actualización</div>');
-		      }
-	      },
-	      beforeSend:function(data){
-	      	$('#update_alert').html('<div class="alert alert-warning">Actualizando...</div>');
-	      }
-	    });
-	}
-	function clean(){
-		ncontrol="";
-		found=false;
-		$('#nc').val("");
-		$('#name').val("");
-		$("#career").val("");
-		$('#age').val("");
-		$('#phone').val("");
-		$('#search_alert').html('');
-	}
-$( document ).ready(function() {
-	var nc=getParameterByName('nc');
-	if (nc!="") {
-		$('#nc').val(nc);
-	    searchByNC(nc);
-	}
-});
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+        $.ajax({
+            type : 'get',
+            url : '{{URL::to('updateStudent')}}',
+            data:vars,
+            success:function(data){
+                if (data=="1") {
+                    $('#update_alert').html('<div class="alert alert-success">Datos actualizados correctamente</div>');
+                    clean();
+                }else{
+                    $('#update_alert').html('<div class="alert alert-warning">Ocurrio un problema con la actualización</div>');
+                }
+            },
+            beforeSend:function(data){
+                $('#update_alert').html('<div class="alert alert-warning">Actualizando...</div>');
+            }
+        });
+    }
+    function clean(){
+        ncontrol="";
+        found=false;
+        $('#nc').val("");
+        $('#name').val("");
+        $("#career").val("");
+        $('#age').val("");
+        $('#phone').val("");
+        $('#search_alert').html('');
+    }
+    $( document ).ready(function() {
+        var nc=getParameterByName('nc');
+        if (nc!="") {
+        $('#nc').val(nc);
+            ncontrol=$('#nc').val();
+            searchByNC(nc);
+        }
+    });
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 </script>
 @stop
